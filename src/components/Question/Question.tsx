3e1de5dec9c09fa589f01a4d2d5acea00b040b20
wenhao <wenhaogoh1998@gmail.com>
2021-03-16 03:05:59 +0800

    Add fonts

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Question.css';
 
 interface Props {
   question?: string;
 }
 
 const Question = ({question}: Props) => {
-  return <div>{question ? <h3>{question}</h3> : null}</div>;
+  return (
+    <div>{question ? <div className={'Question'}>{question}</div> : null}</div>
+  );
 };
 
 export default Question;
